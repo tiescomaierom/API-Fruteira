4f2b4c86c066fc93d8c56927c867e0cd03f087bc
augustomueller <04099635007@senacrs.edu.br>
2026-08-31 20:10:42 -0300

    commit

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace API_Fruteira.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "penis", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
