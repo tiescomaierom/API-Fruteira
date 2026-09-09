97bcb5ba0b5c2c1cd2d1eb507052097ccfcf7569
AUGUSTO MUELLER WENDT <04099635007@senacrs.edu.br>
2026-09-09 19:50:20 -0300

    commit now

---
+++ MODELS/Augusto.cs
@@ -0,0 +1,11 @@
+﻿namespace API_Fruteira.MODELS
+{
+    public class Augusto
+    { 
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade_Estoque { get; set; }
+        public int Categoria_Id { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using MySqlConnector;
+using System.Data;
 
 namespace API_Fruteira
 {
@@ -7,16 +9,15 @@ namespace API_Fruteira
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
+            builder.Services.AddScoped<IDbConnection>((sp) => new MySqlConnection(connectionString));
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -26,7 +27,6 @@ namespace API_Fruteira
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
