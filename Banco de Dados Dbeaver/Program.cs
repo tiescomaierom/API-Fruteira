edc4bf5d2a7e26feeea84d7ebbd26293e575cab9
natyengelmann386-debug <natyengelmann386@gmail.com>
2026-09-09 21:05:30 -0300

    Add files via upload

---
+++ Banco de Dados Dbeaver/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Banco_de_Dados_Dbeaver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
