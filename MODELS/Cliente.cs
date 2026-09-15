da0ad75b1241e6c77b25ad9d3d3525c876e26d54
tiescomaierom <francesco.antoniomaierom@gmail.com>
2026-09-15 20:16:26 -0300

    Commit final

---
+++ MODELS/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace API_Fruteira.MODELS
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Cpf { get; set; }
+        public string Endereco { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# API-Fruteira
+# API-Fruteira realizada em C#
+
+Tabela para rodagem com o script dentro do código na pasta sql.
